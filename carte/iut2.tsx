ca423279de6f816367dff3b1b04e6d1dcf82cdfd
Reda <reda.atik@etu.univ-grenoble-alpes.fr>
2023-11-07 10:15:58 +0100

    carte mis à jour

---
+++ carte/iut2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="iut2" tilewidth="16" tileheight="16" tilecount="1330" columns="35">
+ <image source="../../../Downloads/imageonline-co-pixelated_1.png" trans="ffffff" width="565" height="613"/>
+</tileset>
