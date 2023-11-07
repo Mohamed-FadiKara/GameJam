11dfa0ab15b445dcb44a977b2a0f6e61296de92b
Reda <reda.atik@etu.univ-grenoble-alpes.fr>
2023-11-07 10:23:48 +0100

    carte à jour

---
+++ carte/route.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="route" tilewidth="16" tileheight="16" tilecount="4800" columns="9">
+ <image source="../../../Downloads/route.png" trans="ffffff" width="144" height="192"/>
+</tileset>
