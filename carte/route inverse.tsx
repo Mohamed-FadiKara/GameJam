e17b9aa0547ab59320af530c60bfa98eb5ca758f
Reda <reda.atik@etu.univ-grenoble-alpes.fr>
2023-11-07 15:48:17 +0100

    carte à jour

--- carte/route inverse.tsx
+++ carte/route inverse.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="route inverse" tilewidth="16" tileheight="16" tilecount="108" columns="12">
- <image source="../../../Downloads/route inverse.png" trans="ffffff" width="192" height="144"/>
+ <image source="route inverse.png" trans="ffffff" width="192" height="144"/>
 </tileset>
