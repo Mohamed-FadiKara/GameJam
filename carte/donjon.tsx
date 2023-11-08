5a45178a6c5f0ccd70c01c281d3a0a83aacc6e93
Reda <reda.atik@etu.univ-grenoble-alpes.fr>
2023-11-08 21:26:11 +0100

    donjon ajoute

---
+++ carte/donjon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="donjon" tilewidth="16" tileheight="16" tilecount="48" columns="6">
+ <image source="assets_dungeon.png" trans="ffffff" width="96" height="128"/>
+</tileset>
