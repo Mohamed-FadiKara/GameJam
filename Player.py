30fe4bcf32ceeacbbe46d4ccd3aeaeebb6637584
Mohamed <be65000@gmail.com>
2023-11-08 11:37:00 +0100

    Mouvement joueur

--- Player.py
+++ Player.py
@@ -5,20 +5,38 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self,x,y):
         super().__init__()
-        self.sprite_sheet = pygame.image.load('Joueur/sprit.png')
-        self.image = self.get_image(1,1)
+        self.sprite_sheet = pygame.image.load('Joueur/sprite.png')
+        self.image = self.get_image(0,0)
         self.rect = self.image.get_rect()
         self.image.set_colorkey([0,0,0])
         self.position = [x,y]
+        self.images = {
+            "bas": self.get_image(0, 0),
+            "droite": self.get_image(41, 0),
+            "haut": self.get_image(90, 0),
+            "gauche": self.get_image(134, 0)
+        }
+        #print("Player initialized")
 
-
+        self.speed= 3
+    def change_animation(self,name):
+        self.image = self.images[name]
+        self.image.set_colorkey((0,0,0))
+    def move_droite(self):
+        self.position[0] += self.speed
+    def move_gauche(self):
+        self.position[0] -= self.speed
+    def move_haut(self):
+        self.position[1] -= self.speed
+    def move_bas(self):
+        self.position[1] += self.speed
     def update(self):
         self.rect.topleft = self.position
 
-
     def get_image(self, x,y):
-        image = pygame.Surface([32,64])
-        image.blit(self.sprite_sheet, (0,0),(x,y,32,64))
+        image = pygame.Surface([50,64])
+        image.blit(self.sprite_sheet, (0,0),(x,y,44,64))
+        print(" image du Player ")
         return image
 
 
--- game.py
+++ game.py
@@ -8,31 +8,51 @@ from Player import Player
 class Game:
     def __init__(self):
         #creer la fenetre du jeu
-        self.screen = pygame.display.set_mode(((800,600)))
+        self.screen = pygame.display.set_mode((1000,1000))
         pygame.display.set_caption("IUT2 Adventure")
         tmx_data = pytmx.util_pygame.load_pygame('carte/carte.tmx')
         map_data = pyscroll.data.TiledMapData(tmx_data)
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data,self.screen.get_size())
 
         #GENERER LE JOUEUR
-        self.player = Player(100,50)
+        self.player = Player(30,40)
 
-
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=4)
         self.group.add(self.player)
 
+    def imput(self):
+        bouton = pygame.key.get_pressed()
+        if  bouton[pygame.K_UP]:
+            self.player.move_haut()
+            self.player.change_animation("haut")
+        elif bouton[pygame.K_DOWN]:
+            self.player.move_bas()
+            self.player.change_animation("bas")
+        elif bouton[pygame.K_LEFT]:
+            self.player.move_gauche()
+            self.player.change_animation("gauche")
+        elif bouton[pygame.K_RIGHT]:
+            self.player.move_droite()
+            self.player.change_animation("droite")
+
     def run(self):
+
+        clock = pygame.time.Clock()
+
         running = True
 
         while running:
-
+            self.imput()
             self.group.update()
+            self.group.center(self.player.rect.center)
             self.group.draw(self.screen)
             pygame.display.flip()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+            clock.tick(60)
 
         pygame.quit()
 
